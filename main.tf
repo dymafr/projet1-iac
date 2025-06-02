94b0f5d295d01794f26db2266003477df36c19da
Erwan Vallée <erwanp.vallee@gmail.com>
2025-06-02 16:50:41 +0200

    premier commit

---
+++ main.tf
@@ -0,0 +1,51 @@
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.5.0"
+
+  name = "${var.project_name}-VPC-${terraform.workspace}" // Nom du VPC par environnement
+  cidr = var.vpc_cidr_block
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = terraform.workspace == "prod" ? true : false // NAT Gateway uniquement en prod
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = local.common_tags # Tags communs définis dans locals.tf
+}
+
+# Appel de notre module local 'instance_web'
+module "serveur_web_1" {
+  source = "./modules/instance_web" # Chemin vers notre module
+
+  project_name    = var.project_name
+  environment_tag = terraform.workspace
+  instance_type   = local.current_instance_config.instance_type
+  ami_id          = data.aws_ami.amazon_linux_2023.id
+  vpc_id          = module.vpc.vpc_id
+  subnet_id       = module.vpc.public_subnets[0]
+}
---
+++ outputs.tf
@@ -0,0 +1,25 @@
+
+output "web_server_public_ip" {
+  description = "Adresse IP publique de l'instance EC2 NGINX."
+  value       = module.serveur_web_1.public_ip # Référence la sortie du module
+}
+
+output "web_server_instance_id" {
+  description = "ID de l'instance EC2 NGINX."
+  value       = module.serveur_web_1.instance_id # Référence la sortie du module
+}
+
+output "vpc_id_from_module" {
+  description = "ID du VPC créé par le module distant."
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids_from_module" {
+  description = "Liste des IDs des sous-réseaux publics créés par le module VPC."
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids_from_module" {
+  description = "Liste des IDs des sous-réseaux privés créés par le module VPC."
+  value       = module.vpc.private_subnets
+}
