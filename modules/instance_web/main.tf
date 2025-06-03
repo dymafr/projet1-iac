b1dee196557f03374e50e2e37cd07932d92cea7d
Erwan Vallée <erwanp.vallee@gmail.com>
2025-06-03 09:55:12 +0200

    fix: crit

--- modules/instance_web/main.tf
+++ modules/instance_web/main.tf
@@ -12,15 +12,6 @@ resource "aws_security_group" "web_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  egress {
-    description      = "Allow all outbound traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     Name        = "WebServer-SG-${var.project_name}"
     Project     = var.project_name
