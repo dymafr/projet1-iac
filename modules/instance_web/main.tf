5104be4582a60a6686940af422451a67ff1ac934
Erwan Vallée <erwanp.vallee@gmail.com>
2025-06-03 16:28:52 +0200

    f

--- modules/instance_web/main.tf
+++ modules/instance_web/main.tf
@@ -29,17 +29,17 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file("../../key.pub")
-}
+# resource "aws_key_pair" "my_key" {
+#   key_name   = "my-key"
+#   public_key = file("../../key.pub")
+# }
 
 
 resource "aws_instance" "web_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  key_name      = aws_key_pair.my_key.key_name
+  # key_name      = aws_key_pair.my_key.key_name
 
 
   associate_public_ip_address = true
