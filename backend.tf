901702f122d5bee421d6d54795d33092351c2898
Erwan Vallée <erwanp.vallee@gmail.com>
2025-06-07 14:29:44 +0200

    f

--- backend.tf
+++ backend.tf
@@ -1,12 +1,9 @@
 terraform {
-  backend "s3" {
-    # REMPLACEZ par le nom exact de votre bucket S3 créé pour l'état
-    bucket = "mon-tfstate-bucket-projet1-unique-12345"
+  cloud {
+    organization = "Dyma" // Mettez ici le nom de votre organisation HCP
 
-    key    = "projet1-iac/main/terraform.tfstate" # Chemin du fichier d'état dans le bucket pour ce projet
-    region = "eu-west-3"                          # Région de votre backend
-
-    use_lockfile = true # Utilisation du verrouillage de l'état pour éviter les conflits
-    encrypt      = true # Assure que l'état est chiffré au repos
+    workspaces {
+      name = "projet1-iac" // Nom du workspace que nous allons créer
+    }
   }
 }
